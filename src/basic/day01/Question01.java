79ae1923e20a561ac93eb7a7c0f83892fab6fc49
EstherOVO <tmejwjd@gmail.com>
2024-05-03 14:02:08 +0900

    Programmers 코딩 기초 트레이닝 - 문자열 출력하기

---
+++ src/basic/day01/Question01.java
@@ -0,0 +1,14 @@
+package basic.day01;
+
+import java.util.Scanner;
+
+public class Question01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        System.out.println(a);
+	}
+}
