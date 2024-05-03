b58485ce59402cde437789b040285f68c7cfcb25
EstherOVO <tmejwjd@gmail.com>
2024-05-03 14:05:07 +0900

    Programmers 코딩 기초 트레이닝 - 문자열 붙여서 출력하기

---
+++ src/basic/day02/Question02.java
@@ -0,0 +1,15 @@
+package basic.day02;
+
+import java.util.Scanner;
+
+public class Question02 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		
+		System.out.println(a + b);
+	}
+}
