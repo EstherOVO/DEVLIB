d4e422109243d97f037f8cb88ee767a7b742b45f
EstherOVO <tmejwjd@gmail.com>
2024-05-03 14:03:55 +0900

    Programmers 코딩 기초 트레이닝 - 덧셈식 출력하기

---
+++ src/basic/day02/Question01.java
@@ -0,0 +1,15 @@
+package basic.day02;
+
+import java.util.Scanner;
+
+public class Question01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + " + " + b + " = " + (a + b));
+	}
+}
