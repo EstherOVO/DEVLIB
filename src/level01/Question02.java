6cf999f3f8b9131f017dccebe5f76e347c05c4b8
EstherOVO <tmejwjd@gmail.com>
2024-05-28 14:58:47 +0900

    Programmers - 붕대 감기

--- src/level01/Question02.java
+++ src/level01/Question02.java
@@ -6,53 +6,48 @@ public class Question02 {
 
 //		프로그래머스(Programmers) - PCCP 기출문제 - [PCCP 기출문제] 1번 / 붕대 감기
 		public int solution(int[] bandage, int health, int[][] attacks) {
-			int answer = 0;
 
-			int time = 0;
-			int duration = 0;
-			int max = health;
-			int attackDuration = bandage[0];
-			int recoverHealth = bandage[1];
-			int plusHealth = bandage[2];
+			int lastAttackTime = attacks[attacks.length - 1][0];    // 마지막 공격 시간
+			int max = health;    // 최대 체력
+			int duration = 0;    // 공격 지속 시간
+			int nextAttack = 0;  // 다음 공격
 
-			while (true) {
+			int attackDuration = bandage[0];	// 기술 시전 시간
+			int recoverHealth = bandage[1];		// 초당 회복량
+			int plusHealth = bandage[2];		// 추가 회복량
 
-				for (int i = 0; i < attacks.length; i++) {
+			for (int i = 0; i <= lastAttackTime; i++) {
 
-					int attackTime = attacks[i][0];
-					int damage = attacks[i][1];
+//				현재 시간과 공격 시간이 같을 경우
+				if (i == attacks[nextAttack][0]) {
+					int damage = attacks[nextAttack][1];
+					health -= damage;
+					duration = 0;
 
-					if (attackTime == time) {
-						health -= damage;
+	//				만약 체력이 다 소진되었을 경우, -1 리턴한다.
+					if (health <= 0) {
+						return -1;
+					}
+					nextAttack++;
+
+	//			현재 시간과 공격 시간이 다를 경우
+				} else {
+					health += recoverHealth;
+					duration++;
+
+	//				공격 지속 시간과 기술 시전 시간이 같다면 추가 회복량을 제공하고 공격 지속 시간을 초기화한다.
+					if (duration == attackDuration) {
+						health += plusHealth;
 						duration = 0;
 					}
 
-					if (health <= 0) {
-						return -1;
-					} else {
-						if (time == attackTime && i == attacks.length - 1) {
-							return health;
-						}
-					}
-				}
-
-				health += recoverHealth;
-
-				if (health > max) {
-					health = max;
-				}
-
-				if (duration == attackDuration && time % attackDuration == 0) {
-					health += plusHealth;
+	//				체력이 최대치를 넘지 않도록 한다.
 					if (health > max) {
 						health = max;
 					}
-					duration = 0;
 				}
-
-				duration++;
-				time++;
 			}
+			return health;
 		}
 	}
 
@@ -60,8 +55,8 @@ public class Question02 {
 
 		Solution solution = new Solution();
 
-		int[] bandage1 = { 5, 1, 5 };	// 기술 시전 시간, 초당 회복량, 추가 회복량
-		int[][] attacks1 = { {2, 10}, {9, 15}, {10, 5}, {11, 5} };	// 공격 시간, 피해량
+		int[] bandage1 = { 5, 1, 5 };
+		int[][] attacks1 = { {2, 10}, {9, 15}, {10, 5}, {11, 5} };
 
 		int[] bandage2 = { 3, 2, 7 };
 		int[][] attacks2 = { {1, 15}, {5, 16}, {8, 6} };
